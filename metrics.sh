f40e933f02c41bc5b3c98b08702d2764f1138abe
Confidence9898 <confidenceAI9898@proton.me>
2023-09-01 17:23:41 -0700

    Second Commit

--- metrics.sh
+++
@@ -1,1 +0,0 @@
-python src/utils/val_metrics.py --category lower_body --gen_folder ./results/unpaired --dataset dresscode --dresscode_dataroot ./input --test_order unpaired --batch_size 4 --workers 2
